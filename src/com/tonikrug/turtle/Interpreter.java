121e8ee948ba55c8656b8070ab245397dc87a478
Toni <68860474+caffeineoverflo@users.noreply.github.com>
2024-07-28 18:01:52 +0200

    Created Environment.java

--- src/com/tonikrug/turtle/Interpreter.java
+++ src/com/tonikrug/turtle/Interpreter.java
@@ -1,6 +1,32 @@
 package com.tonikrug.turtle;
 
-public class Interpreter implements Expr.Visitor<Object> {
+import java.util.List;
+
+public class Interpreter implements Expr.Visitor<Object>,
+                                    Stmt.Visitor<Void> {
+
+    private Environment environment = new Environment();
+    void interpret(List<Stmt> statements) {
+        try {
+            for (Stmt statement : statements) {
+                execute(statement);
+            }
+        } catch (RuntimeError error) {
+            Lox.runtimeError(error);
+        }
+    }
+
+    private String stringify(Object object) {
+        if (object == null) return "nil";
+        if (object instanceof Double) {
+            String text = object.toString();
+            if (text.endsWith(".0")) {
+                text = text.substring(0, text.length() - 2);
+            }
+            return text;
+        }
+        return object.toString();
+    }
 
     @Override
     public Object visitLiteralExpr(Expr.Literal expr) {
@@ -19,6 +45,11 @@ public class Interpreter implements Expr.Visitor<Object> {
         }
         // Unreachable.
         return null;
+    }
+
+    @Override
+    public Object visitVariableExpr(Expr.Variable expr) {
+        return environment.get(expr.name);
     }
 
     private void checkNumberOperand(Token operator, Object operand) {
@@ -51,6 +82,59 @@ public class Interpreter implements Expr.Visitor<Object> {
 
     private Object evaluate(Expr expr) {
         return expr.accept(this);
+    }
+
+    private void execute(Stmt stmt) {
+        stmt.accept(this);
+    }
+
+    void executeBlock(List<Stmt> statements,
+                      Environment environment) {
+        Environment previous = this.environment;
+        try {
+            this.environment = environment;
+            for (Stmt statement : statements) {
+                execute(statement);
+            }
+        } finally {
+            this.environment = previous;
+        }
+    }
+
+    @Override
+    public Void visitBlockStmt(Stmt.Block stmt) {
+        executeBlock(stmt.statements, new Environment(environment));
+        return null;
+    }
+
+    @Override
+    public Void visitExpressionStmt(Stmt.Expression stmt) {
+        evaluate(stmt.expression);
+        return null;
+    }
+
+    @Override
+    public Void visitPrintStmt(Stmt.Print stmt) {
+        Object value = evaluate(stmt.expression);
+        System.out.println(stringify(value));
+        return null;
+    }
+
+    @Override
+    public Void visitVarStmt(Stmt.Var stmt) {
+        Object value = null;
+        if (stmt.initializer != null) {
+            value = evaluate(stmt.initializer);
+        }
+        environment.define(stmt.name.lexeme, value);
+        return null;
+    }
+
+    @Override
+    public Object visitAssignExpr(Expr.Assign expr) {
+        Object value = evaluate(expr.value);
+        environment.assign(expr.name, value);
+        return value;
     }
 
     @Override
@@ -94,4 +178,5 @@ public class Interpreter implements Expr.Visitor<Object> {
         // Unreachable.
         return null;
     }
+
 }
--- src/com/tonikrug/turtle/Turtle.java
+++ src/com/tonikrug/turtle/Turtle.java
@@ -1,5 +1,5 @@
 package com.tonikrug.turtle;
-import com.tonikrug.turtle.
+import com.tonikrug.turtle.*;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -10,8 +10,10 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Turtle {
+    private static final Interpreter interpreter = new Interpreter();
     // Indicates if an error has occurred
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
 
     // Entry point of the application
     public static void main(String[] args) throws IOException {
@@ -34,6 +36,7 @@ public class Turtle {
         run(new String(bytes, Charset.defaultCharset()));
         // Exit with error code if an error occurred
         if (hadError) System.exit(65);
+        if (hadRuntimeError) System.exit(70);
     }
 
     // Run the interactive prompt
@@ -55,10 +58,10 @@ public class Turtle {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
         Parser parser = new Parser(tokens);
-        Expr expression = parser.parse();
+        List<Stmt> statements = parser.parse();
         // Stop if there was a syntax error.
         if (hadError) return;
-        System.out.println(new AstPrinter().print(expression));
+        interpreter.interpret(statements);
     }
 
     // Report an error at a specific line
@@ -78,5 +81,11 @@ public class Turtle {
         } else {
             report(token.line,"at '" + token.lexeme + "'", message);
         }
+    }
+
+    static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() +
+                "\n[line " + error.token.line + "]");
+        hadRuntimeError = true;
     }
 }
