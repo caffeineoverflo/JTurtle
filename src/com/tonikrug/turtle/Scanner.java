8e36213c104a9ea903a45a15e143e34da6b2707a
Toni <68860474+caffeineoverflo@users.noreply.github.com>
2024-07-28 14:09:16 +0200

    Fixed small error in scanner.java

--- src/com/tonikrug/turtle/Scanner.java
+++ src/com/tonikrug/turtle/Scanner.java
@@ -106,12 +106,8 @@ class Scanner {
     }
     private void identifier() {
         while (isAlphaNumeric(peek())) advance();
-        String text = source.substring(start, current);
-        TokenType type = keywords.get(text);
-        if (type == null) type = IDENTIFIER;
-        addToken(type);
+        addToken(IDENTIFIER);
     }
-}
     private void number() {
         while (isDigit(peek())) advance();
         // Look for a fractional part.
