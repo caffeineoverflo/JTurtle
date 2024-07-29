00c61e5b3a1f2e45c613a8e4315d469a3253a26c
Toni <68860474+caffeineoverflo@users.noreply.github.com>
2024-07-29 12:14:24 +0200

    Refactor Resolver class and improve scope management
    - Refactor `Resolver` class to enhance readability and maintainability.
    - Improve scope management by adding `beginScope` and `endScope` methods.
    - Add `resolveLocal` method to handle local variable resolution.
    - Enhance error handling for variable initialization and usage.

--- src/com/tonikrug/turtle/TurtleFunction.java
+++ src/com/tonikrug/turtle/TurtleFunction.java
@@ -9,30 +9,31 @@ class TurtleFunction implements TurtleCallable {
         this.closure = closure;
         this.declaration = declaration;
     }
-}
-
-@Override
-public int arity() {
-    return declaration.params.size();
-}
-
-@Override
-public String toString() {
-    return "<fn " + declaration.name.lexeme + ">";
-}
-
-@Override
-public Object call(Interpreter interpreter,
-                   List<Object> arguments) {
-    Environment environment = new Environment(closure);
-    for (int i = 0; i < declaration.params.size(); i++) {
-        environment.define(declaration.params.get(i).lexeme,
-                arguments.get(i));
+    @Override
+    public int arity() {
+        return declaration.params.size();
     }
-    try {
-        interpreter.executeBlock(declaration.body, environment);
-    } catch (Return returnValue) {
-        return returnValue.value;
+
+    @Override
+    public String toString() {
+        return "<fn " + declaration.name.lexeme + ">";
     }
-    return null;
+
+    @Override
+    public Object call(Interpreter interpreter,
+                       List<Object> arguments) {
+        Environment environment = new Environment(closure);
+        for (int i = 0; i < declaration.params.size(); i++) {
+            environment.define(declaration.params.get(i).lexeme,
+                    arguments.get(i));
+        }
+        try {
+            interpreter.executeBlock(declaration.body, environment);
+        } catch (Return returnValue) {
+            return returnValue.value;
+        }
+        return null;
+    }
 }
+
+
